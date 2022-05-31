0bb6ac57e7e169be655ad230fcef475c56ea5a31
Shubham Khan <shubhamkhanit@gmail.com>
2022-06-01 00:22:52 +0530

    initial setup

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+const Expenses = (props) => {
+    return (
+        <Card className="expenses">
+            {props.items.map((expense) => (
+                <ExpenseItem
+                    key = {expense.id}
+                    title = {expense.title}
+                    amount = {expense.amount}
+                    date = {expense.date}
+                />
+            ))}
+        </Card>
+    )
+}
+
+export default Expenses;
