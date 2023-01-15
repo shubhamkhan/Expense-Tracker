ad4dcb08c5de94de83739afa1cbecb4508854cb4
Shubham Khan <shubhamkhanit@gmail.com>
2023-01-15 10:30:46 +0530

    Update app data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -70,7 +70,7 @@ const ExpenseForm = (props) => {
                     <label>Date</label>
                     <input
                         type="date"
-                        min="2022-01-01"
+                        min="2019-01-01"
                         max="2025-12-31"
                         value={enteredDate}
                         onChange={dateChangeHandler}
